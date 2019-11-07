3791df16540d5f4e594dc86becb06213d1e7c1be
FinalDayz <steef2000d@gmail.com>
2019-11-07 12:44:17 +0100

    fixed DAO

--- src/main/java/com/udemy/persistence/ExperimentDetailsDAO.java
+++ src/main/java/com/udemy/persistence/ExperimentDetailsDAO.java
@@ -20,18 +20,6 @@ public interface ExperimentDetailsDAO {
     @SqlUpdate("DELETE FROM experiment_details  WHERE experiment_id = :id")
     public void delete(@Bind("id") int id);
 
-        ExperimentDetails experimentDetails2 = new ExperimentDetails();
-        experimentDetails2.setArchief(true);
-        experimentDetails2.setArchiefType("HOF");
-        experimentDetails2.setStatus("Experiment goed uitgevoerd");
-        experimentDetails2.setBeschrijving("Dit is een mensenlijk experiment, van kinderen van 5 jaar tot volwassenen tot 95 jaar");
-        experimentDetails2.setDoorlooptijd("10 jaar");
-        experimentDetails2.setExperimentId(1);
-        experimentDetails2.setKostenAnders("1000000 euro voor nieuwe organen en andere");
-        experimentDetails2.setKostenInovatie("100000 voor mnedicatie");
-        experimentDetails2.setNetwerk("Europa en rusland");
-        experimentDetails2.setStatusKleur("Rood");
-        experimentDetails2.setVoortgang("Het gaat slecht met de gezondheid van de deelnemers");
 
     public void add(ExperimentDetails newExperimentDetail);
 
@@ -49,42 +37,4 @@ public interface ExperimentDetailsDAO {
             " WHERE experiment_ID = :id")
     public void update(@Bind("id") int id, @BindBean ExperimentDetails updatedExperimentDetails);
 
-    public List<ExperimentDetails> getAll() {
-        return this.experimentsDetails;
-    }
-
-    public ExperimentDetails find(int id) {
-        /* EXECUTE QUERY */
-        for (ExperimentDetails experimentDetails : this.experimentsDetails) {
-            if (experimentDetails.getExperimentId() == id) {
-                return experimentDetails;
-            }
-        }
-        return null;
-    }
-
-    public void delete(int id) {
-        /* EXECUTE QUERY */
-        for (ExperimentDetails experimentDetails : this.experimentsDetails) {
-            if (experimentDetails.getExperimentId() == id) {
-                experimentsDetails.remove(experimentDetails);
-                return;
-            }
-        }
-    }
-
-    public void add(ExperimentDetails newExperimentDetail) {
-        /* EXECUTE QUERY */
-        this.experimentsDetails.add(newExperimentDetail);
-    }
-
-    public void update(int id, ExperimentDetails updatedExperimentDetails) {
-        /* EXECUTE QUERY */
-        for (ExperimentDetails experimentDetails : this.experimentsDetails) {
-            if (experimentDetails.getExperimentId() == id) {
-                experimentsDetails.set(experimentsDetails.indexOf(experimentDetails), updatedExperimentDetails);
-                return;
-            }
-        }
-    }
 }
